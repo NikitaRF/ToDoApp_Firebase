1622dfea47a79a38da15efa749439e05d249c508
nikitabeloshapka <existing@live.ru>
2021-05-26 12:57:19 +0500

    Mac

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -2,7 +2,7 @@ import React, { useReducer, useContext } from 'react'
 import { Alert } from 'react-native'
 import { TodoContext } from './todoContext'
 import { todoReducer } from './todoReducer'
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import {ADD_TODO, CLEAR_ERROR, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO} from '../types'
 import {ScreenContext} from "../screen/screenContext";
 
 
@@ -13,13 +13,27 @@ export const TodoState = ({ children }) => {
     error: null,
   }
 
+  const showLoader = () => dispatch({type: SHOW_LOADER})
+  const hideLoader = () => dispatch({type: HIDE_LOADER})
+  const showError = (error) => ({type: SHOW_ERROR, error})
+  const clearError = () => ({type: CLEAR_ERROR})
 
 
   const {changeScreen} = useContext(ScreenContext)
 
   const [state, dispatch] = useReducer(todoReducer, initialState)
 
-  const addTodo = title => dispatch({ type: ADD_TODO, title })
+  const addTodo = async title => {
+      const response = await fetch('https://todoapp-aa2ea-default-rtdb.europe-west1.firebasedatabase.app/todos.json', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({title})
+      })
+      const data = await response.json()
+      console.log(data)
+
+      dispatch({ type: ADD_TODO, title, id: data.name })
+  }
 
   const removeTodo = id => {
       const todo = state.todos.find(t => t.id === id)
